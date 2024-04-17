31dd3ca752b783c8a4e6e88f494a29bbf9c38059
Teo <teccent@yandex.ru>
2024-04-17 20:09:15 +0300

    Initial Commit

---
+++ ContactListAppSwiftUI/ContactListAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListAppSwiftUIApp.swift
+//  ContactListAppSwiftUI
+//
+//  Created by Теона Магай on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
